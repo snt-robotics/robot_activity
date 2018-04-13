4d82f88f7cd41584063752bb6ba13e97327977c5
Maciej Zurad <maciej.zurad@gmail.com>
2018-04-13 13:22:28 +0200

    migrating to package.xml v2 and adding roslint to robot_process_tutorials

--- robot_process_tutorials/include/robot_process_tutorials/robot_process_tutorials.h
+++ robot_process_tutorials/include/robot_process_tutorials/robot_process_tutorials.h
@@ -1,12 +1,47 @@
-#ifndef ROBOT_PROCESS_TUTORIALS_H
-#define ROBOT_PROCESS_TUTORIALS_H
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, University of Luxembourg
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of University of Luxembourg nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Maciej Zurad
+ *********************************************************************/
+#ifndef ROBOT_PROCESS_TUTORIALS_ROBOT_PROCESS_TUTORIALS_H
+#define ROBOT_PROCESS_TUTORIALS_ROBOT_PROCESS_TUTORIALS_H
 
 #include <ros/ros.h>
 #include <robot_process/managed_robot_process.h>
 
 #include <robot_process_msgs/State.h>
 #include <std_srvs/Empty.h>
-
 
 namespace robot_process_tutorials
 {
@@ -38,6 +73,6 @@ private:
   int counter = 0;
 };
 
-} // namespace robot_process_tutorials
+}  // namespace robot_process_tutorials
 
-#endif
+#endif  // ROBOT_PROCESS_TUTORIALS_ROBOT_PROCESS_TUTORIALS_H
